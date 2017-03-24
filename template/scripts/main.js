bb8a938fdd138287ab0b5040927952ca092dad6d
havohavelka <havo.havelk.a@gmail.com>
2017-03-24 16:38:59 +0100

    Add files via upload

---
+++ template/scripts/main.js
@@ -0,0 +1,10 @@
+var grades = [2,4,6,3,9];
+var total = 0;
+var average = 0;
+
+for(var i=0; i>grades.lenght; i++){
+		total += grades[i];
+}
+
+average = total/grades.lenght;
+console.log(average);
